9bbdc725bb12da3f744b18e24a4e7e150e10d5b5
letuan <letuan2372000@gmail.com>
2022-09-27 11:40:33 +0700

    testt

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
-app.get('/', function (req, res) {
+app.get('/test', function (req, res) {
   res.send('Hello World!');
 });
 app.listen(3000, function () {
   console.log('ok now port 3000!');
 });
+
